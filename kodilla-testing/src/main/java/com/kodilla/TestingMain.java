eb2f3d0d6f9d179d6f45b35e671fdddd08392b32
Nicki Rafal <nicol79@op.pl>
2023-06-04 09:42:41 +0200

    Nowe_testy_jednostkowe

--- kodilla-testing/src/main/java/com/kodilla/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla;
-
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calcutator;
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("Testowanie użytkownika");
+        String pom = simpleUser.getUsername();
+        if (pom.equals("Testowanie użytkownika")){
+            System.out.println("Test ok");
+        }else {
+            System.out.println( "Test Error");
+        }
+        Calcutator calcutator = new Calcutator();
+        // Test dla metody add(int a, int b)
+        int a = 5;
+        int b = 3;
+        int expectedResult = 8;
+        int result = calcutator.add(5,3);
+        System.out.println("Wynik dodawania: " + result);
+        if (result == expectedResult) {
+            System.out.println("Test dla metody add() przeszedł pomyślnie.");
+        } else {
+            System.out.println("Test dla metody add() nie przeszedł. Oczekiwany wynik: " + expectedResult);
+        }
+
+        // Test dla metody subtract(int a, int b)
+        a = 10;
+        b = 4;
+        expectedResult = 6;
+        result = calcutator.subtract(10,4);
+        System.out.println("Wynik odejmowania: " + result);
+        if (result == expectedResult) {
+            System.out.println("Test dla metody subtract() przeszedł pomyślnie.");
+        } else {
+            System.out.println("Test dla metody subtract() nie przeszedł. Oczekiwany wynik: " + expectedResult);
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calcutator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calcutator {
+
+
+    public int add(int a, int b){
+         int c = a+b;
+         return c;
+    }
+    public int subtract (int a, int b){
+        int c = a-b;
+        return  c;
+
+    }
+}
