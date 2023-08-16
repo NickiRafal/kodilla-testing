7d3bb8b10939cfaea2244a53b624cedd26f22039
RafalNicki <Nicol79@op.pl>
2023-08-16 13:20:09 +0200

    [15.1]  java 17

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class LoggerTestSuite {
+    @Test
+    void testLastLog(){
+        //Given
+        Logger logger = Logger.INSTANCE;
+        //When
+        logger.log("Log1");
+        //Then
+        assertEquals("Log1",logger.getLastLog());
+    }
+    @Test
+    void testSingleton() {
+        // Given
+        Logger logger1 = Logger.INSTANCE;
+        Logger logger2 = Logger.INSTANCE;
+
+        // When
+        logger1.log("Log from logger1");
+        logger2.log("Log from logger2");
+
+        // Then
+        assertEquals("Log from logger2", logger1.getLastLog());
+        assertEquals("Log from logger2", logger2.getLastLog());
+    }
+}
